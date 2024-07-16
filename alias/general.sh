8f1f183f088c9ef2706ceac620ededb05342b553
Marcelo Michels <marcelo@michels.app>
2024-07-15 21:38:56 -0300

    fix lssize

--- alias/general.sh
+++ alias/general.sh
@@ -1,6 +1,6 @@
 
 # General
 alias ll="ls -lhSa"
-alias lssize="du -sh *"
-alias llsize="du -sh *"
+alias lssize="du -sh .[^.]* *"
+alias llsize="du -sh .[^.]* *"
 alias previsaoTempo="curl 'https://wttr.in/?1'"
