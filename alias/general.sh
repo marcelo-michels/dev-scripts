39423716ab9fae7399e1028dc8e71c89f4b2b188
Marcelo  Michels <mm@Marcelos-MacBook-Pro.local>
2025-04-24 08:33:23 -0300

    feat: create openFullScreenUrl

--- alias/general.sh
+++ alias/general.sh
@@ -4,3 +4,7 @@ alias ll="ls -lhSa"
 alias lssize="du -sh .[^.]* *"
 alias llsize="du -sh .[^.]* *"
 alias previsaoTempo="curl 'https://wttr.in/?1'"
+
+function openFullScreenUrl {
+  open -a "Google Chrome" --args --kiosk "$1"
+}
